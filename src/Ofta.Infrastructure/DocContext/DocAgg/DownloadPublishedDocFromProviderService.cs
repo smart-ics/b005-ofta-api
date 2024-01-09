9ae8a0aa6771f47605172315df16319adadb350a
Jude <drury_yl@yahoo.com>
2024-01-09 16:13:24 +0700

    Melengkapi TODO List

--- src/Ofta.Infrastructure/DocContext/DocAgg/DownloadPublishedDocFromProviderService.cs
+++ src/Ofta.Infrastructure/DocContext/DocAgg/DownloadPublishedDocFromProviderService.cs
@@ -4,8 +4,12 @@ namespace Ofta.Infrastructure.DocContext.DocAgg;
 
 public class DownloadPublishedDocFromProviderService : IDownloadPublishedDocFromProviderService
 {
+    //  TODO: Implement DownloadPublishedDocFromProviderService
+    //      Download dokumen dari server TekenAJa dengan menggunakan DocumentId
+    //      yang didapat dari response SendToSignProviderService
+    //      Lalu simpan dokumen tersebut ke folder yang ditentukan
     public void Execute(DownloadPublishedDocFromProviderRequest req)
     {
-        throw new NotImplementedException();
+        return;
     }
 }
