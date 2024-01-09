d2affd7033e0fabf89e75695d59da359f8f0e337
Jude <drury_yl@yahoo.com>
2024-01-09 07:52:21 +0700

    Pemberian catatan TODO inftastructure

--- src/Ofta.Infrastructure/DocContext/DocAgg/GetContentBase64Service.cs
+++ src/Ofta.Infrastructure/DocContext/DocAgg/GetContentBase64Service.cs
@@ -5,6 +5,8 @@ namespace Ofta.Infrastructure.DocContext.DocAgg;
 public class GetContentBase64Service : IGetContentBase64Service
 {
     //  TODO: Implement GetContentBase64Service
+    //      Open file, read content lalu ubah content tersebut jadi string base64
+    //      dan kembalikan string base64 tersebut
     public string Execute(string filePathName)
     {
         return string.Empty;
