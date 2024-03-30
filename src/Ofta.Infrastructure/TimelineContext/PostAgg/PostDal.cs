6396bc0555b6f285aaef13cfdcf7ff02f5603044
Jude <drury_yl@yahoo.com>
2024-03-30 08:59:42 +0700

    Develop DAL Post Aggregate

--- src/Ofta.Infrastructure/TimelineContext/PostAgg/PostDal.cs
+++ src/Ofta.Infrastructure/TimelineContext/PostAgg/PostDal.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Options;
+using Nuna.Lib.DataAccessHelper;
 using Nuna.Lib.ValidationHelper;
 using Ofta.Application.TImelineContext.PostAgg.Contracts;
 using Ofta.Domain.TImelineContext.PostAgg;
@@ -18,26 +22,106 @@ public class PostDal : IPostDal
 
     public void Insert(PostModel model)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            INSERT INTO OFTA_Post (
+                PostId, PostDate, UserOftaId,
+                Msg, DocId, CommentCount, LikeCount) 
+            VALUES(
+                @PostId, @PostDate, @UserOftaId,
+                @Msg, @DocId, @CommentCount, @LikeCount)";
+
+        var dp = new DynamicParameters();
+        dp.AddParam("@PostId", model.PostId, SqlDbType.VarChar); 
+        dp.AddParam("@PostDate", model.PostDate, SqlDbType.DateTime); 
+        dp.AddParam("@UserOftaId", model.UserOftaId, SqlDbType.VarChar);
+        dp.AddParam("@Msg", model.Msg, SqlDbType.VarChar);
+        dp.AddParam("@DocId", model.DocId, SqlDbType.VarChar); 
+        dp.AddParam("@CommentCount", model.CommentCount, SqlDbType.VarChar); 
+        dp.AddParam("@LikeCount", model.LikeCount, SqlDbType.VarChar);
+
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        conn.Execute(sql, dp);
     }
 
     public void Update(PostModel model)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            UPDATE
+                OFTA_Post
+            SET
+                PostDate = @PostDate, 
+                UserOftaId = @UserOftaId,
+                Msg = @Msg, 
+                DocId = @DocId, 
+                CommentCount = @CommentCount, 
+                LikeCount = @LikeCount 
+            WHERE
+                PostId = @PostId ";
+
+        var dp = new DynamicParameters();
+        dp.AddParam("@PostId", model.PostId, SqlDbType.VarChar); 
+        dp.AddParam("@PostDate", model.PostDate, SqlDbType.DateTime); 
+        dp.AddParam("@UserOftaId", model.UserOftaId, SqlDbType.VarChar);
+        dp.AddParam("@Msg", model.Msg, SqlDbType.VarChar);
+        dp.AddParam("@DocId", model.DocId, SqlDbType.VarChar); 
+        dp.AddParam("@CommentCount", model.CommentCount, SqlDbType.VarChar); 
+        dp.AddParam("@LikeCount", model.LikeCount, SqlDbType.VarChar);
+
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        conn.Execute(sql, dp);
     }
 
     public void Delete(IPostKey key)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            DELETE FROM
+                OFTA_Post
+            WHERE
+                PostId = @PostId ";
+
+        var dp = new DynamicParameters();
+        dp.AddParam("@PostId", key.PostId, SqlDbType.VarChar); 
+
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        conn.Execute(sql, dp);
     }
 
     public PostModel GetData(IPostKey key)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            SELECT
+                PostId, PostDate, UserOftaId,
+                Msg, DocId, CommentCount, LikeCount 
+            FROM
+                OFTA_Post
+            WHERE
+                PostId = @PostId ";
+
+        var dp = new DynamicParameters();
+        dp.AddParam("@PostId", key.PostId, SqlDbType.VarChar); 
+
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        return conn.ReadSingle<PostModel>(sql, dp);
     }
 
     public IEnumerable<PostModel> ListData(Periode filter1, IUserOftaKey filter2)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            SELECT
+                PostId, PostDate, UserOftaId,
+                Msg, DocId, CommentCount, LikeCount 
+            FROM
+                OFTA_Post
+            WHERE
+                PostDate BETWEEN @tgl1 AND @tgl2
+                AND UserOftaId = @userOftaId ";
+
+        var dp = new DynamicParameters();
+        dp.AddParam("@tgl1", filter1.Tgl1, SqlDbType.DateTime); 
+        dp.AddParam("@tgl2", filter1.Tgl2, SqlDbType.DateTime); 
+        dp.AddParam("@userOftaId", filter2.UserOftaId, SqlDbType.VarChar); 
+
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        return conn.Read<PostModel>(sql, dp);
     }
 }
---
+++ src/Ofta.Infrastructure/TimelineContext/PostAgg/PostVisibilityDal.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Options;
+using Nuna.Lib.DataAccessHelper;
+using Ofta.Application.TImelineContext.PostAgg.Contracts;
+using Ofta.Domain.TImelineContext.PostAgg;
+using Ofta.Infrastructure.Helpers;
+
+namespace Ofta.Infrastructure.TimelineContext.PostAgg;
+
+public class PostVisibilityDal : IPostVisibilityDal
+{
+    private readonly DatabaseOptions _opt;
+
+    public PostVisibilityDal(IOptions<DatabaseOptions> opt)
+    {
+        _opt = opt.Value;
+    }
+
+    public void Insert(IEnumerable<PostVisibilityModel> listModel)
+    {
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        using var bcp = new SqlBulkCopy(conn);
+        conn.Open();
+        
+        bcp.AddMap("PostId", "PostId");
+        bcp.AddMap("VisibilityReff", "VisibilityReff");
+        
+
+        var fetched = listModel.ToList();
+        bcp.BatchSize = fetched.Count;
+        bcp.DestinationTableName = "OFTA_PostVisibility";
+        bcp.WriteToServer(fetched.AsDataTable());
+        conn.Close();
+    }
+
+    public void Delete(IPostKey key)
+    {
+        const string sql = @"
+            DELETE FROM
+                OFTA_PostVisibility
+            WHERE
+                PostId = @PostId";
+        
+        var dp = new DynamicParameters();
+        dp.AddParam("@PostId", key.PostId, SqlDbType.VarChar);
+        
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        conn.Execute(sql, dp);
+    }
+
+    public IEnumerable<PostVisibilityModel> ListData(IPostKey filter)
+    {
+        const string sql = @"
+            SELECT
+                aa.PostId, aa.VisibilityReff
+            FROM
+                OFTA_PostVisibility aa
+            WHERE
+                aa.PostId = @PostId";
+        
+        var dp = new DynamicParameters();
+        dp.AddParam("@PostId", filter.PostId, SqlDbType.VarChar);
+        
+        using var conn = new SqlConnection(ConnStringHelper.Get(_opt));
+        return conn.Query<PostVisibilityModel>(sql, dp);
+    }
+}
