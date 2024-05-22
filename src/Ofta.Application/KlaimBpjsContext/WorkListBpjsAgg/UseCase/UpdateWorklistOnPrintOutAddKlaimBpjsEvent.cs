5251d1609df7ff6079cad9658a6f776e0efef681
renka nara <renka.narra@gmail.com>
2024-05-22 15:25:42 +0700

    add udpate work state saat print doc

---
+++ src/Ofta.Application/KlaimBpjsContext/WorkListBpjsAgg/UseCase/UpdateWorklistOnPrintOutAddKlaimBpjsEvent.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Ofta.Application.KlaimBpjsContext.KlaimBpjsAgg.UseCases;
+using Ofta.Application.KlaimBpjsContext.OrderKlaimBpjsAgg.Workers;
+using Ofta.Application.KlaimBpjsContext.WorkListBpjsAgg.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ofta.Application.KlaimBpjsContext.WorkListBpjsAgg.UseCase;
+
+
+public class UpdateWorklistOnPrintOutAddKlaimBpjsEvent : INotificationHandler<PrintedDocKlaimBpjsEvent>
+{
+    private readonly IWorkListBpjsBuilder _builder;
+    private readonly IWorkListBpjsWriter _writer;
+
+    public UpdateWorklistOnPrintOutAddKlaimBpjsEvent(IWorkListBpjsBuilder builder,
+        IWorkListBpjsWriter writer)
+    {
+        _builder = builder;
+        _writer = writer;
+    }
+
+    public Task Handle(PrintedDocKlaimBpjsEvent notification, CancellationToken cancellationToken)
+    {
+        var workListBpjs = _builder
+            .Load(notification.Aggregate)
+            .WorkState(notification.Aggregate.KlaimBpjsState)
+            .Build();
+
+        _ = _writer.Save(workListBpjs);
+
+        return Task.CompletedTask;
+    }
+}
