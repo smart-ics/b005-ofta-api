e9b39192bf92cee8a8df9aaf06f6b034f2b3e9c4
renka nara <renka.narra@gmail.com>
2024-05-22 10:47:35 +0700

    fix update kode klaimbpjsid ke ofta_worklist

---
+++ src/Ofta.Application/KlaimBpjsContext/WorkListBpjsAgg/UseCase/UpdateWorklistOnCreatedKlaimBpjsEvent.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Ofta.Application.KlaimBpjsContext.KlaimBpjsAgg.UseCases;
+using Ofta.Application.KlaimBpjsContext.WorkListBpjsAgg.Workers;
+
+namespace Ofta.Application.KlaimBpjsContext.WorkListBpjsAgg.UseCase;
+
+public class UpdateWorklistOnCreatedKlaimBpjsEvent : INotificationHandler<CreatedKlaimBpjsEvent>
+{
+    private readonly IWorkListBpjsBuilder _builder;
+    private readonly IWorkListBpjsWriter _writer;
+
+    public UpdateWorklistOnCreatedKlaimBpjsEvent(IWorkListBpjsBuilder builder,
+        IWorkListBpjsWriter writer)
+    {
+        _builder = builder;
+        _writer = writer;
+    }
+
+    public Task Handle(CreatedKlaimBpjsEvent notification, CancellationToken cancellationToken)
+    {
+        var workListBpjs = _builder
+            .Load(notification.Aggregate)
+            .KlaimBpjs(notification.Aggregate)
+            .Build();
+
+        _ = _writer.Save(workListBpjs);
+
+        return Task.CompletedTask;
+    }
+}
