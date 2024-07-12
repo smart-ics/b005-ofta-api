e45469248ceb60b6c57a7e27b0312655d1f3337f
renka nara <renka.narra@gmail.com>
2024-07-12 16:28:31 +0700

    fix klaim bpjs state dan fix bug doc url kosong masih termerger

--- src/Ofta.Application/KlaimBpjsContext/KlaimBpjsAgg/UseCases/Event/CreateDoc_OnKlaimBpjsMergerFileEventHandler.cs
+++ src/Ofta.Application/KlaimBpjsContext/KlaimBpjsAgg/UseCases/Event/CreateDoc_OnKlaimBpjsMergerFileEventHandler.cs
@@ -1,7 +1,6 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 using MediatR;
-using Ofta.Application.DocContext.DocAgg.Contracts;
 using Ofta.Application.DocContext.DocAgg.Workers;
 using Ofta.Application.KlaimBpjsContext.KlaimBpjsAgg.Workers;
 using Ofta.Application.ParamContext.ConnectionAgg.Contracts;
@@ -19,39 +18,41 @@ public class CreateDoc_OnKlaimBpjsMergerFileEventHandler
     private readonly IDocBuilder _docBuilder;
     private readonly IDocWriter _docWriter;
     private readonly IKlaimBpjsWriter _klaimBpjsWriter;
-    private readonly IWriteFileService _writeFileService;
     private readonly IParamSistemDal _paramSistemDal;
 
     public CreateDoc_OnKlaimBpjsMergerFileEventHandler(IDocBuilder docBuilder,
         IDocWriter docWriter,
         IKlaimBpjsWriter klaimBpjsWriter,
-        IWriteFileService writeFileService,
         IParamSistemDal paramSistemDal)
     {
         _docBuilder = docBuilder;
         _docWriter = docWriter;
         _klaimBpjsWriter = klaimBpjsWriter;
-        _writeFileService = writeFileService;
         _paramSistemDal = paramSistemDal;
     }
 
     public Task Handle(MergerFileBpjsEvent notification, CancellationToken cancellationToken)
     {
 
+        var agg = notification.Agg;
+        var cmd = notification.Command;
+
+        // GUARD
         var paramStoragePath = _paramSistemDal.GetData(Sys.LocalStoragePath)
             ?? throw new KeyNotFoundException("Parameter StoragePath not found");
         var paramStorageUrl = _paramSistemDal.GetData(Sys.LocalStorageUrl)
             ?? throw new KeyNotFoundException("Parameter StorageUrl not found");
 
-        //  create Document atas file yg sudah di-print
-        var agg = notification.Agg;
-        var cmd = notification.Command;
-
         var printOuts = agg.ListDocType
             .SelectMany(x => x.ListPrintOut)
-            .Where(printOut => !string.IsNullOrEmpty(printOut.DocUrl))
-            ?? throw new ArgumentException("Document not found");
+            .Where(printOut => !string.IsNullOrEmpty(printOut.DocUrl?.Trim())).ToList();
 
+        if (!printOuts.Any())
+        {
+            throw new ArgumentException("Document not found");
+        }
+
+        // BUILD
         var fallback = Policy<DocModel>
             .Handle<KeyNotFoundException>()
             .Fallback(() => _docBuilder
@@ -63,7 +64,7 @@ public class CreateDoc_OnKlaimBpjsMergerFileEventHandler
         var doc = fallback.Execute(() =>
             _docBuilder.Load(new DocModel(agg.MergerDocId)).Build());
 
-        //  WRITE
+        // WRITE
         //      doc
         doc = _docBuilder
             .Attach(doc)
@@ -83,12 +84,13 @@ public class CreateDoc_OnKlaimBpjsMergerFileEventHandler
         //      mergerFile;       
         var files = agg.ListDocType
             .SelectMany(x => x.ListPrintOut)
-            .Where(printOut => !string.IsNullOrEmpty(printOut.DocUrl))
+            .Where(printOut => !string.IsNullOrEmpty(printOut.DocUrl?.Trim()))
             .Select(printOut => printOut.DocUrl.Replace(paramStorageUrl.ParamSistemValue, paramStoragePath.ParamSistemValue))
             .ToList();
 
         var result = agg.MergerDocUrl.Replace(paramStorageUrl.ParamSistemValue, paramStoragePath.ParamSistemValue);
         MergePdfFiles(files, result);
+
         return Task.CompletedTask;
     }
 
