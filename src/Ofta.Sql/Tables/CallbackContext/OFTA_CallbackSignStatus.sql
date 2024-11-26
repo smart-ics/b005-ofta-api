d22fb98b4213bc7e62be160f88ebaed423bffa9c
Albertus Khrisna <akhrisna32@gmail.com>
2024-11-26 13:20:28 +0700

    refactor: add column JsonPayload on OFTA_CallbackSignStatus

--- src/Ofta.Sql/Tables/CallbackContext/OFTA_CallbackSignStatus.sql
+++ src/Ofta.Sql/Tables/CallbackContext/OFTA_CallbackSignStatus.sql
@@ -4,6 +4,7 @@ CREATE TABLE OFTA_CallbackSignStatus(
   Email VARCHAR(50) NOT NULL CONSTRAINT DF_OFTA_CallbackSignStatus_Email DEFAULT(''),
   TilakaName VARCHAR(50) NOT NULL CONSTRAINT DF_OFTA_CallbackSignStatus_TilakaName DEFAULT (''),
   CallbackDate VARCHAR(20) NOT NULL CONSTRAINT DF_OFTA_CallbackSignStatus_CallbackDate DEFAULT('3000-01-01 00:00:00'),
+  JsonPayload VARCHAR(MAX) NOT NULL CONSTRAINT DF_OFTA_CallbackSignStatus_JsonPayload DEFAULT (''),
 )
 GO
 
